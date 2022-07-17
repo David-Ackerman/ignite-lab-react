bd1fd33be69f47dfbe6d85c0a2330b073eda1798
David_Ackerman <david_pereira-16@hotmail.com>
2022-07-17 16:27:02 -0300

    finish class 5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      slug
-      availableAt
-      title
-    }
-  }
-`;
-
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: "live" | "class";
-  }[];
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+  const { data } = useGetLessonsQuery();
 
-  console.log(data);
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600 min-h-screen">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -43,12 +20,6 @@ export function Sidebar() {
             type={lesson.lessonType}
           />
         ))}
-        <Lesson
-          title="Aula 01"
-          slug="aula-01"
-          availableAt={new Date()}
-          type="class"
-        />
       </div>
     </aside>
   );
